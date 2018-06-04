78001da7ad2e93ad1ca0f5de03e95bb8704e8e0d
krish <krishnan167@gmail.com>
2018-06-04 15:46:24 +0800

    preloader code updates

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ class App  extends Component {
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
     this.getProfileData = this.getProfileData.bind(this);
-
     this.state = {
       profileList: [],
       activeIndex: null,
       currentPage: 1,
       nextPage: null,
-      prevPage: null
+      prevPage: null,
+      loading: false
     };
-
   }
 
-  componentDidMount() {
+  getInitialState() {
+    return { loading: false };
+  }
+
+  componentWillMount() {
     this.getProfileData();
   }
 
@@ -34,7 +37,8 @@ class App  extends Component {
               profileList: myJSON.results,
               activeIndex: myJSON.results.length ? 0 : null,
               nextPage: myJSON.next,
-              prevPage: myJSON.previous
+              prevPage: myJSON.previous,
+              loading: false
           });
       })
   }
@@ -66,9 +70,11 @@ class App  extends Component {
   increment = ()=>{
     if(this.state.nextPage!=null){
       this.setState({
-          currentPage: this.state.currentPage+1
+          currentPage: this.state.currentPage+1,
+          loading: true
       }, function(){
         this.getProfileData();
+        this.setState.loading = false;
       })
     } else {
       return false;
@@ -78,9 +84,11 @@ class App  extends Component {
   decrement() {
     if (this.state.prevPage!=null) {
       this.setState({
-          currentPage: this.state.currentPage-1
+          currentPage: this.state.currentPage-1,
+          loading: true
       }, function(){
         this.getProfileData();
+        this.setState.loading = false;
       })
     } else{
       return false;
@@ -88,74 +96,79 @@ class App  extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Star War</h1>
-        </header>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="listStyle01">
-                <ul className="list-group">
-                  {this.renderProfList()}
-                </ul>
-              </div>
-              <nav aria-label="Page navigation" className="paginationStyle01">
-                <div className="actionBtn">
-                  <ul className="pagination">
-                    <li className="page-item">
-                      <a className="page-link" aria-label="Previous" onClick={this.decrement.bind(this)} disabled={!this.state.prevPage} value="prev">
-                        <span>Prev</span>
-                      </a>
-                    </li>
-                    <li className="page-item">
-                      <a className="page-link" aria-label="Next" onClick={this.increment.bind(this)} disabled={!this.state.nextPage} value="next">
-                        <span>Next</span>
-                      </a>
-                    </li>
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Star War</h1>
+          </header>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="listStyle01">
+                  <ul className="list-group">
+                    {this.renderProfList()}
                   </ul>
                 </div>
-                <div className="pagnInfo">
-                  You are in page no: <strong>{this.state.currentPage}</strong>
-                </div>
-              </nav>
-            </div>
-            <div className="col-md-8">
-              <div>
-                   {!this.state.profileList.length === 0 && (
-                    <span>No data found</span>
-                   )}
-
-                   {this.state.profileList.length > 0 && (
+                <nav aria-label="Page navigation" className="paginationStyle01">
+                  <div className="actionBtn">
+                    <ul className="pagination">
+                      <li className="page-item">
+                        <a className="page-link" aria-label="Previous" onClick={this.decrement.bind(this)} disabled={!this.state.prevPage || this.state.loading.toString() === 'true'} value="prev">
+                          <span>Prev</span>
+                        </a>
+                      </li>
+                      <li className="page-item">
+                        <a className="page-link" aria-label="Next" onClick={this.increment.bind(this)} disabled={!this.state.nextPage || this.state.loading.toString() === 'true'} value="next">
+                          <span>Next</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="pagnInfo">
+                    You are in page no: <strong>{this.state.currentPage}</strong>
+                  </div>
+                </nav>
+              </div>
+              <div className="col-md-8">
+                <div>
+                     {!this.state.profileList.length === 0 && (
+                      <span>No data found</span>
+                     )}
                      <div className="profileDetails"> 
-                       <div className="row">
-                        <div className="profilePic"> 
-                          <img src={require('./images/profile.png')} alt={this.state.profileList[this.state.activeIndex].name} width="256" height="256" />
-                        </div>
-                        <div className="profPrimaryInfo">
-                          <h4> {this.state.profileList[this.state.activeIndex].name} </h4>
-                          <h6> Gender: {this.state.profileList[this.state.activeIndex].gender} </h6>
-                          <h6> Height: {this.state.profileList[this.state.activeIndex].height} cm </h6>
-                          <h6> Weight: {this.state.profileList[this.state.activeIndex].mass} kg </h6>
-                          <h6> Year of birth: {this.state.profileList[this.state.activeIndex].birth_year}  </h6>
-                          <h6> Hair color: {this.state.profileList[this.state.activeIndex].hair_color}  </h6>
-                          <h6> Skin color: {this.state.profileList[this.state.activeIndex].skin_color}  </h6>
-                          <h6> Eye color: {this.state.profileList[this.state.activeIndex].eye_color}  </h6>
-                        </div>
-                        </div>
-                         <div className="row rowSytlw01">
-                           <p>profile short description profile short description profile short description profile short description profile short description profile short description profile short description ...</p>
-                         </div>
+                        {this.state.loading.toString() === 'true' && (
+                          <div className="loadingStyle01">loading...</div>
+                        )} 
+
+                        {this.state.profileList.length > 0 && this.state.loading.toString() !== 'true' && (
+                          <div>
+                             <div className="row">
+                              <div className="profilePic"> 
+                                <img src={require('./images/profile.png')} alt={this.state.profileList[this.state.activeIndex].name} width="256" height="256" />
+                              </div>
+                              <div className="profPrimaryInfo">
+                                <h4> {this.state.profileList[this.state.activeIndex].name} </h4>
+                                <h6> Gender: {this.state.profileList[this.state.activeIndex].gender} </h6>
+                                <h6> Height: {this.state.profileList[this.state.activeIndex].height} cm </h6>
+                                <h6> Weight: {this.state.profileList[this.state.activeIndex].mass} kg </h6>
+                                <h6> Year of birth: {this.state.profileList[this.state.activeIndex].birth_year}  </h6>
+                                <h6> Hair color: {this.state.profileList[this.state.activeIndex].hair_color}  </h6>
+                                <h6> Skin color: {this.state.profileList[this.state.activeIndex].skin_color}  </h6>
+                                <h6> Eye color: {this.state.profileList[this.state.activeIndex].eye_color}  </h6>
+                              </div>
+                              </div>
+                               <div className="row rowSytlw01">
+                                 <p>profile short description profile short description profile short description profile short description profile short description profile short description profile short description ...</p>
+                               </div>
+                            </div> 
+                          )}
                      </div>
-                   )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    )
-  }
+      )
+     }
 }
 
 
